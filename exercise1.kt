f27b2ec143648c9ca866e7c6311f43a11d75f509
SusSargsyan <100150526+SusSargsyan@users.noreply.github.com>
2022-02-27 21:46:16 +0400

    Add files via upload

---
+++ exercise1.kt
@@ -0,0 +1,13 @@
+/* Write a program which will output follwing
+ * xxxx
+ * x  x
+ * x  x
+ * xxxx
+ */
+
+fun main() {
+    println("xxxx")
+    println("x  x")
+    println("x  x")
+    println("xxxx")
+}
