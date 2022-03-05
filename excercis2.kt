f2285786cedf28caf0b8103911926e3e0fb9c2eb
SusSargsyan <100150526+SusSargsyan@users.noreply.github.com>
2022-03-05 18:21:08 +0400

    Create excercis2.kt

---
+++ excercis2.kt
@@ -0,0 +1,26 @@
+// Write a program which will output 10 times follwing
+// xxxx
+// x  x
+// x  x
+// xxxx
+//
+
+fun main() {
+    foo()  // 1
+    foo()  // 2
+    foo()  // 3
+    foo()  // 4
+    foo()  // 5
+    foo()  // 6
+    foo()  // 7
+    foo()  // 8
+    foo()  // 9
+    foo()  // 10
+    }
+
+fun foo() {
+    println("xxxx")
+    println("x  x")
+    println("x  x")
+    println("xxxx")
+}
