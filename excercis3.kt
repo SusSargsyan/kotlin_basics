4fb0ba9e634d2e2cdc4359bf1fc913fb91e92921
SusSargsyan <100150526+SusSargsyan@users.noreply.github.com>
2022-03-05 18:22:37 +0400

    Create excercis3.kt

---
+++ excercis3.kt
@@ -0,0 +1,13 @@
+// var - variable
+// Output the sum of squares of given two numbers
+
+fun main() {
+ 	var a :Int = 5
+    var b :Int = 4
+    
+    println(a)
+    println(b)
+    println("Output the sum of squares of given two numbers")
+    println(a * a + b * b)/ : - column
+
+}
