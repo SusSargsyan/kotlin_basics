b182482f6a4a57c15706405137c23abf7fafc3bd
SusSargsyan <100150526+SusSargsyan@users.noreply.github.com>
2022-04-22 22:03:38 +0400

    Create textbook2.kt

---
+++ textbook2.kt
@@ -0,0 +1,20 @@
+import kotlin.math.pow
+import kotlin.math.sin
+
+/**
+ * Textbook - 2 problem
+ */
+
+fun main() {
+	val x:Int = 3
+    val y:Int = 6
+    
+    val part1 = x.toDouble().pow(2.0)
+    val part2 = part1 - 4
+    val part3 = y.toDouble().pow(2.0)
+    val part4 = part3 + 2
+   
+    
+    val result = part2 / part4
+    
+}
