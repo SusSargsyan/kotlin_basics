f602f2cac63b1019ac6032489b8a7c2afa721b55
SusSargsyan <100150526+SusSargsyan@users.noreply.github.com>
2022-04-23 21:47:49 +0400

    Update textbook3.kt

--- textbook3.kt
+++ textbook3.kt
@@ -1,6 +1,7 @@
 import kotlin.math.pow
-import kotlin.math.ctg
+import kotlin.math.tan
 import kotlin.math.log
+
 
 
 /**
@@ -12,13 +13,15 @@ fun main() {
     val y:Int = 8
     
     val part1 = x.toDouble().pow(2.0)
-    val part2 = y.toDouble().pow(2.0) 
+	val part2 = y.toDouble().pow(2.0) 
     val part3 = part1 + part2
-    val part4 = log(part1 + 1)
-    val part5 = |part1 - y|
-   
+    val part4 = part1-y
+    val part5 = part4 / part3
+    val part6 = 1/tan(part5) 
+    val part7 = part1 + 1
+  
     
-    val result = part5 / part3 + part4
+    val result = part6 + part7
     print(result)
     
 }
