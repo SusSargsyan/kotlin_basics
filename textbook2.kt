d6d47e4280c00f1e81d0560acb81f9b04aca26d5
SusSargsyan <100150526+SusSargsyan@users.noreply.github.com>
2022-04-23 20:57:54 +0400

    Update textbook2.kt

--- textbook2.kt
+++ textbook2.kt
@@ -13,9 +13,13 @@ fun main() {
     val part2 = part1 - 4
     val part3 = y.toDouble().pow(2.0)
     val part4 = part3 + 2
+    val part5 = part1 + 1
+    val part6 = part5.toDouble().pow(0.5)
+    val part7 = sin(part5)
+    val part8 = 2.toDouble().pow(part7)
    
     
-    val result = part2 / part4
+    val result = part2 / part4 + part8
     print(result)
     
 }
