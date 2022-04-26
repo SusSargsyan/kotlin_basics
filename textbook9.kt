6c2f88a084146a4d6b21401df4c431846c3eff0e
SusSargsyan <100150526+SusSargsyan@users.noreply.github.com>
2022-04-26 20:55:44 +0400

    Update textbook9.kt

--- textbook9.kt
+++ textbook9.kt
@@ -4,22 +4,25 @@ import kotlin.math.sin
 
 
 /**
- * Textbook - 7 problem
+ * Textbook - 9 problem
  */
 
 fun main() {
 	val x:Int = 7
-    val y:Int = 3
-    val z:Int = 1
+    val y:Int = 5
     
-    val part1 = x.toDouble().pow(2.0)
-    val part2 = sin(x + z)
-    val part3 = part2.toDouble().pow(2.0)
-    val part4 = 3 * z
     
-   
+    val part1 = x.toDouble().pow(2.0) 
+    val part2 = y.toDouble().pow(2.0)
+    val part3 = x + 4 
+    val part4 = part2 + 4
+    val part5 = part4.toDouble().pow(3.0)
+    val part6 = part3 / part5
+    val part7 = part6.toDouble().pow(1 / 5) 
+    val part8 = sin(x + part7.toDouble().pow(2.0))
+    val part9 = 3 * part7
+    val part10 = y / part1
     
-    val result = part3 + part4 + y/part1
+    val result = part8 + part9 + part10
     print(result)
-    
 }
