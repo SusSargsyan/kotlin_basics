dd189380be513a62017a9ebc01eba0cf5ffd512d
SusSargsyan <100150526+SusSargsyan@users.noreply.github.com>
2022-04-26 20:41:53 +0400

    Update textbook7.kt

--- textbook7.kt
+++ textbook7.kt
@@ -8,18 +8,21 @@ import kotlin.math.cos
  */
 
 fun main() {
-	val x:Int = 7
+    val x:Int = 7
     val y:Int = 3
     
-    val part1 = x.toDouble().pow(2.0)
-    val part2 = y.toDouble().pow(2.0) 
-    val part3 = part1 + part2
-    val part4 = part3 + 4
-    val part5 = part4.toDouble().pow(7.0)
-    val part6 =  sin(cos(x + y))
-   
     
-    val result = part5 + part6
+    val part1 = x.toDouble().pow(2.0) 
+    val part2 = y.toDouble().pow(2.0)
+    val part3 = part1 + part2
+    val part4 = part3.toDouble().pow(5.0)
+    val part5 = part4 + 4
+    val part6 = part5.toDouble().pow(7.0) 
+    val part7 = x.toDouble() + y.toDouble()
+    val part8 = cos(part7)
+    val part9 = sin(part8)
+    
+    val result = part6 + part9
     print(result)
     
 }
