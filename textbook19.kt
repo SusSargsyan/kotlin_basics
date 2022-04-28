beb9e6ef8105d29f838e62f7860d1e6eb818ef29
SusSargsyan <100150526+SusSargsyan@users.noreply.github.com>
2022-04-28 20:23:45 +0400

    Create textbook19.kt

---
+++ textbook19.kt
@@ -0,0 +1,50 @@
+import kotlin.math.pow
+import kotlin.math.log10
+import kotlin.math.atan
+
+/**
+ * Textbook - 19 problem
+ */
+
+fun exercise19(a:Int, b:Int): Double {
+    var result:Double = 0.0
+    val cond:Double = a.toDouble() + b.toDouble()
+    
+    if(cond < 3) {
+         val part1 =  atan(a.toDouble() + b.toDouble())
+         val part2 =  part1.toDouble().pow(4.0)
+         result = part2
+        
+    } else if(cond > 5) {
+         val part1 = log10(a.toDouble() + b.toDouble())
+         val part2 = part1.toDouble().pow(4.0) 
+         result = part2
+    } else {
+         val part1 = a.toDouble().pow(15.0)
+         result = part1
+    }
+   
+    return 0.0
+}
+
+
+fun main() {
+	
+    var a:Int = -7
+    var b:Int = 2
+    val result1 = exercise19(a, b)
+    println(result1)
+    
+   
+    a = 6
+    b = 1
+    val result2 = exercise19(a, b)
+    println(result2)
+    
+  
+    a = 9
+    b = 7
+    val result3 = exercise19(a, b)
+    println(result3)
+    
+}
