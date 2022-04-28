b0fd8b8e81762246b4886df92f135e3eb9c6e313
SusSargsyan <100150526+SusSargsyan@users.noreply.github.com>
2022-04-28 20:10:13 +0400

    Create textbook18.kt

---
+++ textbook18.kt
@@ -0,0 +1,54 @@
+import kotlin.math.pow
+import kotlin.math.abs
+import kotlin.math.cos
+import kotlin.math.abs
+import kotlin.math.atan
+
+/**
+ * Textbook - 18 problem
+ */
+
+fun exercise18(x:Int, a:Int, b:Int): Double {
+    var result:Double = 0.0
+    val cond:Double = a.toDouble() + abs(b.toDouble())
+    var e:Int = 1
+    if(cond < -5) {
+        val part1 =  cos(a.toDouble() + x.toDouble() + b.toDouble())
+        val part2 =  part1.toDouble().pow(2.0)
+        val part3 = abs(a + x)
+        val part4 = e.toDouble().pow(part3)
+        result = part4 * part2
+        
+    } else if(cond > 2) {
+        val part1 = atan(a.toDouble() + x.toDouble())
+        val part2 = part1.toDouble().pow(1 / 3) 
+        result = part2
+    } else {
+        val part1 = a.toDouble() + abs(b)
+        result = part1
+    }
+   
+    return 0.0
+}
+
+
+fun main() {
+	var x:Int = 1
+    var a:Int = -8
+    var b:Int = 2
+    val result1 = exercise18(x, a, b)
+    println(result1)
+    
+    x = 5
+    a = 7
+    b = 3
+    val result2 = exercise18(x, a, b)
+    println(result2)
+    
+    x = 5
+    a = 1
+    b = 1
+    val result3 = exercise18(x, a, b)
+    println(result3)
+    
+}
