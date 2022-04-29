2cdd30e302c06a40b894ec2ec5121ce410239b85
SusSargsyan <100150526+SusSargsyan@users.noreply.github.com>
2022-04-29 18:42:14 +0400

    Create exercise34.kt

---
+++ exercise34.kt
@@ -0,0 +1,24 @@
+// Write a program which calculates sum  of squares
+// from 1..n numbers with function returning the sum
+
+
+fun sum(n:Int):Int {
+    var n:Int = 10
+    var sum:Int = 0
+    var a:Int = 1
+    for(i in 1..11) {
+        println("${i - 1}^2 = $a")
+    	a = a + i
+        sum = sum + a
+    }
+        
+    
+	return sum
+    
+}
+
+fun main() {
+    var n:Int = 10
+	println(sum(10))
+   
+}
