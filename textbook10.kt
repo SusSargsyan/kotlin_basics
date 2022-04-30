b260be39b8ab9dc63605fd392ad73a920a689d39
SusSargsyan <100150526+SusSargsyan@users.noreply.github.com>
2022-04-30 22:33:45 +0400

    Update textbook10.kt

--- textbook10.kt
+++ textbook10.kt
@@ -1,28 +1,33 @@
 import kotlin.math.pow
 import kotlin.math.sin
 import kotlin.math.cos
-
-
+import kotlin.math.PI
+import kotlin.math.exp
 
 /**
  * Textbook - 10 problem
  */
-
-fun main() {
-  	val x:Int = 8
-    val y:Int = 2
-    val p:Int = 180
-    val e:Int = 3
-    
-    
+fun exercise10(x:Int, y:Int):Double {
     val part1 = x.toDouble().pow(2.0) 
     val part2 = sin(part1 + y)
     val part3 = part2.toDouble().pow(2.0) 
-    val part4 = sin(p / 12 + x.toDouble())
-    val part5 = cos(y.toDouble() + part3 / p + part3.toDouble().pow(2.0) )
+    val part4 = sin(PI / 12 + x.toDouble())
+    val part5 = cos((y.toDouble() + part3) / (PI + part3.toDouble().pow(2.0)))
     val part6 = part3 + 4
-    val part7 = e.toDouble().pow(part6) 
+    val part7 = exp(part6) 
     
     val result = part4 * part5 + part7
-    print(result)
+    return result
+}
+
+fun main() {
+  	var x:Int = 8
+    var y:Int = 2
+    var result = exercise10(x, y)
+    println(result)
+    
+    x = 1
+    y = 4
+    result = exercise10(x, y)
+    println(result)
 }
