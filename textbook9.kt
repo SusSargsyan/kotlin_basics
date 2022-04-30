6a7ef214a4599177535c58506c7bcbbb533ee11e
SusSargsyan <100150526+SusSargsyan@users.noreply.github.com>
2022-04-30 22:40:48 +0400

    Update textbook9.kt

--- textbook9.kt
+++ textbook9.kt
@@ -1,17 +1,10 @@
 import kotlin.math.pow
 import kotlin.math.sin
 
-
-
 /**
  * Textbook - 9 problem
  */
-
-fun main() {
-	val x:Int = 7
-    val y:Int = 5
-    
-    
+fun exercise9(x:Int, y:Int): Double {
     val part1 = x.toDouble().pow(2.0) 
     val part2 = y.toDouble().pow(2.0)
     val part3 = x + 4 
@@ -19,10 +12,22 @@ fun main() {
     val part5 = part4.toDouble().pow(3.0)
     val part6 = part3 / part5
     val part7 = part6.toDouble().pow(1 / 5) 
-    val part8 = sin(x + part7.toDouble().pow(2.0))
+    val part8 = sin(x + part7).toDouble().pow(2.0)
     val part9 = 3 * part7
     val part10 = y / part1
     
     val result = part8 + part9 + part10
-    print(result)
+    return result
+}
+
+fun main() {
+	var x:Int = 7
+    var y:Int = 5
+    var result = exercise9(x, y)
+    println(result)
+    
+    x = 2
+    y = 6
+    result = exercise9(x, y)
+    println(result)
 }
