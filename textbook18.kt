f6f27c8af6d5d0c60be3a8c79dd6e1f161d347bb
SusSargsyan <100150526+SusSargsyan@users.noreply.github.com>
2022-04-30 23:16:09 +0400

    Update textbook18.kt

--- textbook18.kt
+++ textbook18.kt
@@ -3,6 +3,7 @@ import kotlin.math.abs
 import kotlin.math.cos
 import kotlin.math.abs
 import kotlin.math.atan
+import kotlin.math.exp
 
 /**
  * Textbook - 18 problem
@@ -11,14 +12,12 @@ import kotlin.math.atan
 fun exercise18(x:Int, a:Int, b:Int): Double {
     var result:Double = 0.0
     val cond:Double = a.toDouble() + abs(b.toDouble())
-    var e:Int = 1
     if(cond < -5) {
         val part1 =  cos(a.toDouble() + x.toDouble() + b.toDouble())
         val part2 =  part1.toDouble().pow(2.0)
         val part3 = abs(a + x)
-        val part4 = e.toDouble().pow(part3)
+        val part4 = exp(part3.toDouble())
         result = part4 * part2
-        
     } else if(cond > 2) {
         val part1 = atan(a.toDouble() + x.toDouble())
         val part2 = part1.toDouble().pow(1 / 3) 
@@ -27,13 +26,11 @@ fun exercise18(x:Int, a:Int, b:Int): Double {
         val part1 = a.toDouble() + abs(b)
         result = part1
     }
-   
-    return 0.0
+    return result
 }
 
-
 fun main() {
-	var x:Int = 1
+    var x:Int = 1
     var a:Int = -8
     var b:Int = 2
     val result1 = exercise18(x, a, b)
@@ -50,5 +47,4 @@ fun main() {
     b = 1
     val result3 = exercise18(x, a, b)
     println(result3)
-    
 }
