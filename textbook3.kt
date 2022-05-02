bbe430eb6ad0aa589dce634ff84a429c2a831384
SusSargsyan <100150526+SusSargsyan@users.noreply.github.com>
2022-05-02 21:37:43 +0400

    Update textbook3.kt

--- textbook3.kt
+++ textbook3.kt
@@ -3,16 +3,11 @@ import kotlin.math.tan
 import kotlin.math.log10
 import kotlin.math.abs
 
-
-
 /**
  * Textbook - 3 problem
  */
 
-fun main() {
-	val x:Int = 2
-    val y:Int = 8
-    
+fun exercise3(x:Int, y:Int): Double {
     val part1 = x.toDouble().pow(2.0)
 	val part2 = y.toDouble().pow(2.0) 
     val part3 = part1 + part2
@@ -20,7 +15,18 @@ fun main() {
     val part5 = part4 / part3
     val part6 = 1 / tan(part5) 
     val part7 = log10(part1 + 1)
+    val result = part6 + part7
+    return result
+}
+
+fun main() {
+	var x:Int = 2
+    var y:Int = 8
+    var result = exercise3(x, y)
+    println(result)
     
-	val result = part6 + part7
-    print(result)
+    x = 1
+    y = 3
+    result = exercise3(x, y)
+    println(result)
 }
