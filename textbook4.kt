99943cd2d5d1eebe52b3bc0b8c635ebc155d5382
SusSargsyan <100150526+SusSargsyan@users.noreply.github.com>
2022-05-02 21:42:41 +0400

    Update textbook4.kt

--- textbook4.kt
+++ textbook4.kt
@@ -7,15 +7,23 @@ import kotlin.math.ln
  * Textbook - 4 problem
  */
 
-fun main() {
-	val x:Int = 3
-    val y:Int = 6
-    
+fun exercise4(x:Int, y:Int): Double {
     val part1 = x.toDouble().pow(2.0) + 4
     val part2 = part1.toDouble().pow(1 / 3)
     val part3 = exp(x.toDouble()) + 1
     val part4 = ln(part3)
-    
     val result = part4 + part2
-    print(result)
+    return result
+}
+
+fun main() {
+	var x:Int = 3
+    var y:Int = 6
+    var result = exercise4(x, y)
+    println(result)
+    
+    x = 7
+    y = 4
+    result = exercise4(x, y)
+    println(result)
 }
