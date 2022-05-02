6d66f72fd6db4ebc50d21aca158712d2ad5f08f0
SusSargsyan <100150526+SusSargsyan@users.noreply.github.com>
2022-05-02 22:00:59 +0400

    Update textbook7.kt

--- textbook7.kt
+++ textbook7.kt
@@ -2,16 +2,11 @@ import kotlin.math.pow
 import kotlin.math.sin
 import kotlin.math.cos
 
-
 /**
  * Textbook - 7 problem
  */
 
-fun main() {
-    val x:Int = 7
-    val y:Int = 3
-    
-    
+fun exercise7(x:Int, y:Int): Double {
     val part1 = x.toDouble().pow(2.0) 
     val part2 = y.toDouble().pow(2.0)
     val part3 = part1 + part2
@@ -21,8 +16,18 @@ fun main() {
     val part7 = x.toDouble() + y.toDouble()
     val part8 = cos(part7)
     val part9 = sin(part8)
-    
     val result = part6 + part9
-    print(result)
+    return result 
+}
+
+fun main() {
+    var x:Int = 7
+    var y:Int = 3
+    var result = exercise7(x, y)
+    println(result)
     
+    x = 9
+    x = 1
+    result = exercise7(x, y)
+    println(result)
 }
