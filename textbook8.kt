50567069209b0a3213c06abcbbbbb9830d5a3b58
SusSargsyan <100150526+SusSargsyan@users.noreply.github.com>
2022-05-03 19:14:06 +0400

    Update textbook8.kt

--- textbook8.kt
+++ textbook8.kt
@@ -1,28 +1,31 @@
 import kotlin.math.pow
-import kotlin.math.sin
-import kotlin.math.tan
-
+import kotlin.math.abs
 
 /**
- * Textbook - 1 problem
+ * Textbook - 8 problem
  */
-fun exercise1(x:Int, y:Int): Double {
-    val part1 = x.toDouble().pow(2.0)
-    val part2 = part1.pow(6.0)
-    val part3 = sin(part1 - 3)
-    val part4 = tan(y.toDouble())
-    val result = (y + 1) * (x + part2 * part3 - part4)
+
+fun exercise8(x:Int, y:Int): Double {
+	val part1 = x.toDouble().pow(2.0)
+    val part2 = y.toDouble().pow(2.0)
+    val part3 = part2 + 4
+    val part4 = part3.toDouble().pow(1 / 3)
+    val part5 = part1 + part4
+    val part6 = part5.toDouble().pow(1 / 4)
+    val part7 = abs(x) + abs(y)
+    val part8 = part7.toDouble().pow(10.0)
+    val result = part6 + part8
     return result  
 }
 
 fun main() {
 	var x:Int = 2
     var y:Int = 5
-    var result = exercise1(x, y)
+    var result = exercise8(x, y)
     println(result)
     
     x = 3
     y = 1
-    result = exercise1(x, y)
+    result = exercise8(x, y)
     println(result)
 }
