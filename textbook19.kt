96cf0b07c1c6de695f76061861c15dd50a73c50b
SusSargsyan <100150526+SusSargsyan@users.noreply.github.com>
2022-05-03 18:57:49 +0400

    Update textbook19.kt

--- textbook19.kt
+++ textbook19.kt
@@ -1,5 +1,5 @@
 import kotlin.math.pow
-import kotlin.math.log10
+import kotlin.math.log2
 import kotlin.math.atan
 
 /**
@@ -9,24 +9,21 @@ import kotlin.math.atan
 fun exercise19(a:Int, b:Int): Double {
     var result:Double = 0.0
     val cond:Double = a.toDouble() + b.toDouble()
-    
     if(cond < 3) {
          val part1 =  atan(a.toDouble() + b.toDouble())
          val part2 =  part1.toDouble().pow(4.0)
          result = part2
-        
     } else if(cond > 5) {
-         val part1 = log10(a.toDouble() + b.toDouble())
-         val part2 = part1.toDouble().pow(4.0) 
+         val part1 = a.toDouble() + b.toDouble()
+         val part2 = log2(part1) / log2(8.0)
+         val part3 = part2.toDouble().pow(4.0) 
          result = part2
     } else {
          val part1 = a.toDouble().pow(15.0)
          result = part1
     }
-   
-    return result
+   return result
 }
-
 
 fun main() {
 	
@@ -35,16 +32,14 @@ fun main() {
     val result1 = exercise19(a, b)
     println(result1)
     
-   
     a = 6
     b = 1
     val result2 = exercise19(a, b)
     println(result2)
     
-  
     a = 9
     b = 7
     val result3 = exercise19(a, b)
     println(result3)
-    
 }
+        
