5c54330f57182b67e3c1feb6c34aa8f4f14a8cd5
SusSargsyan <100150526+SusSargsyan@users.noreply.github.com>
2022-05-04 21:33:27 +0400

    Create textbook37.kt

---
+++ textbook37.kt
@@ -0,0 +1,14 @@
+/**
+ * Textbook - 37 problem
+ */
+
+fun main() {
+	var a1:Int = 1
+    var a2:Int = 3
+    var a3:Int = 5
+    if(a2 >= a1 + 2) {
+        println("true")
+    } else {
+        println("false")
+    }
+}
