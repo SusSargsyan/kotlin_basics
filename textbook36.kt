91a3bfdce9d10fdbde9fb07c7ed33fdf3caf2b45
SusSargsyan <100150526+SusSargsyan@users.noreply.github.com>
2022-05-04 21:20:38 +0400

    Create textbook36.kt

---
+++ textbook36.kt
@@ -0,0 +1,16 @@
+/**
+ * Textbook - 36 problem
+ */
+
+fun main() {
+	var a:Int = 2
+    var b:Int = 5
+    var c:Int = 6
+    if(c % 2 == 0) {
+        println("1")
+    } else if(a % 2 == 0) {
+        println("1")
+    } else {
+        println("2")
+    }
+}
