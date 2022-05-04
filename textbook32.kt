4678efecd8efce8cf43747ec4362b779cccea97d
SusSargsyan <100150526+SusSargsyan@users.noreply.github.com>
2022-05-04 19:43:10 +0400

    Create textbook32.kt

---
+++ textbook32.kt
@@ -0,0 +1,16 @@
+/**
+ * Textbook - 32 problem
+ */
+
+fun main() {
+	val a:Int = 6
+    val b:Int = 4
+    val c:Int = 1
+    if(c < a) {
+        println(c)
+    } else if(b < c) {
+        println(b)
+    } else {
+        println(a)
+    }
+}
