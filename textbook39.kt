60f00572a42337ec0469b91bb98163201878e4e3
SusSargsyan <100150526+SusSargsyan@users.noreply.github.com>
2022-05-04 22:12:44 +0400

    Create textbook39.kt

---
+++ textbook39.kt
@@ -0,0 +1,14 @@
+/**
+ * Textbook - 39 problem
+ */
+
+fun main() {  
+	var a:Int = 3
+    var b:Int = 6
+    var c:Int = 9
+    if(a < b && b < c) {
+        println("3, 6, 9")
+    } else {
+        println("false")
+    }
+}
