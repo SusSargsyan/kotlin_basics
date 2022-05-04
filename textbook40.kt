e5b611b034ea12bf39456af0ffd1af10e28cfcfe
SusSargsyan <100150526+SusSargsyan@users.noreply.github.com>
2022-05-04 22:14:35 +0400

    Create textbook40.kt

---
+++ textbook40.kt
@@ -0,0 +1,14 @@
+/**
+ * Textbook - 40 problem
+ */
+
+fun main() {  
+	var a:Int = 10
+    var b:Int = 8
+    var c:Int = 3
+    if(a > b && b > c) {
+        println("10, 8, 3")
+    } else {
+        println("false")
+    }
+}
