f0b9e57fb61b1301875f3b494083ed2e29ee2438
SusSargsyan <100150526+SusSargsyan@users.noreply.github.com>
2022-05-04 20:40:57 +0400

    Create textbook33.kt

---
+++ textbook33.kt
@@ -0,0 +1,18 @@
+/**
+ * Textbook - 33 problem
+ */
+
+fun main() {
+	var a:Int = 6
+    var b:Int = 4
+    var c:Int = 1
+    if(c >= 1) {
+        println("true")
+    } else if(b >= 1) {
+        println("true")
+    } else if(a >= 1) {
+        println("true")
+    } else {
+        println("false")
+    }
+}
