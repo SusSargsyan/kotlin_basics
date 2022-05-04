e57835f50acb9ad300837a21f3a545396f465f5c
SusSargsyan <100150526+SusSargsyan@users.noreply.github.com>
2022-05-04 22:02:40 +0400

    Create textbook38.kt

---
+++ textbook38.kt
@@ -0,0 +1,14 @@
+/**
+ * Textbook - 38 problem
+ */
+
+fun main() {  
+	  var a1:Int = 2
+    var a2:Int = 4
+    var a3:Int = 8
+    if(a2 >= a1 * 2) {
+        println("true")
+    } else {
+        println("false")
+    }
+}
