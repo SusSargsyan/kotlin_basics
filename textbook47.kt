23c3427f5c2132846d0bbdfbb4d339701ccffbe4
SusSargsyan <100150526+SusSargsyan@users.noreply.github.com>
2022-05-06 22:22:53 +0400

    Create textbook47.kt

---
+++ textbook47.kt
@@ -0,0 +1,20 @@
+/**
+ * Textbook - 47 problem
+ */
+
+fun main() {
+    val a1:Int = 7
+    val a2:Int = 9
+    val a3:Int = 11
+    val a4:Int = 13
+    val d:Int = 2
+          if(a2 == a1 + d) {
+          println("true")
+    } else if(a3 == a2 + d){
+          println("true")
+    } else if(a4 == a3 + d) {
+        println("true")
+   } else {
+        println("false")
+   }
+}
