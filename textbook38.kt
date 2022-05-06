61ba64eaf227a24d8f5e5328b494f53ad65af17e
SusSargsyan <100150526+SusSargsyan@users.noreply.github.com>
2022-05-06 21:55:04 +0400

    Update textbook38.kt

--- textbook38.kt
+++ textbook38.kt
@@ -3,10 +3,10 @@
  */
 
 fun main() {  
-	  var a1:Int = 2
+    var a1:Int = 2
     var a2:Int = 4
     var a3:Int = 8
-    if(a2 >= a1 * 2) {
+    if(a2 == a1 * 2) {
         println("true")
     } else {
         println("false")
