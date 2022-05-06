7b094d6b1709f2127d3497516037b372107eb393
SusSargsyan <100150526+SusSargsyan@users.noreply.github.com>
2022-05-06 22:46:06 +0400

    Create textbook49.kt

---
+++ textbook49.kt
@@ -0,0 +1,15 @@
+/**
+ * Textbook - 49 problem
+ */
+
+fun main() {
+    val a:Int = 2
+    val b:Int = 4
+    val c:Int = 8
+    val d:Int = 16
+    if(a < b && b < c && c < d) {
+          println("2, 4, 8, 16")
+   } else {
+        println("false")
+   }
+}
