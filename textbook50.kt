2813e26b78189d74392b47b406c97bc1d30cbbd2
SusSargsyan <100150526+SusSargsyan@users.noreply.github.com>
2022-05-06 22:49:03 +0400

    Create textbook50.kt

---
+++ textbook50.kt
@@ -0,0 +1,15 @@
+/**
+ * Textbook - 50 problem
+ */
+
+fun main() {
+    val a:Int = 15
+    val b:Int = 9
+    val c:Int = 6
+    val d:Int = 3
+    if(a > b && b > c && c > d) {
+          println("15, 9, 6, 3")
+   } else {
+        println("false")
+   }
+}
