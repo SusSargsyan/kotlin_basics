8021e7eb0ac7cae0f802ecfb4852add9a04d81b2
SusSargsyan <100150526+SusSargsyan@users.noreply.github.com>
2022-05-06 21:49:57 +0400

    Create textbook46.kt

---
+++ textbook46.kt
@@ -0,0 +1,15 @@
+/**
+ * Textbook - 46 problem
+ */
+
+fun main() {
+    val a:Int = 7
+    val b:Int = 2
+    val c:Int = 5
+    val d:Int = 1
+   if(c % 2 == 1 && a % 2 == 1) {
+          println("1")
+    } else {
+          println("2")
+    }
+}
