b0e594a1c7a4da0309d537331a4885acdf5e2fb6
SusSargsyan <100150526+SusSargsyan@users.noreply.github.com>
2022-05-06 21:23:13 +0400

    Create textbook41.kt

---
+++ textbook41.kt
@@ -0,0 +1,19 @@
+/**
+ * Textbook - 41 problem
+ */
+
+fun main() {
+    val a:Int = 3
+    val b:Int = 8
+    val c:Int = 6
+    val d:Int = 7
+    if(a < b) {
+        println(b)
+    } else if(c < a) {
+          println(a)
+    } else if(d > c) {
+          println(d)
+    } else {
+          println(a)
+    }
+}
