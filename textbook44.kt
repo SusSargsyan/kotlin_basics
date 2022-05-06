d7ba643424dcf85d61e02cc105d1d4805e9aa7b6
SusSargsyan <100150526+SusSargsyan@users.noreply.github.com>
2022-05-06 21:34:11 +0400

    Create textbook44.kt

---
+++ textbook44.kt
@@ -0,0 +1,15 @@
+/**
+ * Textbook - 44 problem
+ */
+
+fun main() {
+    val a:Int = 3
+    val b:Int = 2
+    val c:Int = 4
+    val d:Int = 1
+   if(a + b <= c + d) {
+          println(true)
+    } else {
+          println(false)
+    }
+}
