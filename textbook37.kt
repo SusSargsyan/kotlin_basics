de626cdc9bdf5a274ce33d03448ad335325252a3
SusSargsyan <100150526+SusSargsyan@users.noreply.github.com>
2022-05-06 22:35:46 +0400

    Update textbook37.kt

--- textbook37.kt
+++ textbook37.kt
@@ -6,7 +6,8 @@ fun main() {
 	var a1:Int = 1
     var a2:Int = 3
     var a3:Int = 5
-    if(a2 == a1 + 2) {
+    var d:Int = 2
+    if(a2 == a1 + d) {
         println("true")
     } else {
         println("false")
