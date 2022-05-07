6a206ea749802fc035d99e287776d998713304f2
SusSargsyan <100150526+SusSargsyan@users.noreply.github.com>
2022-05-07 20:00:53 +0400

    Create textbook23.kt

---
+++ textbook23.kt
@@ -0,0 +1,37 @@
+import kotlin.math.pow
+
+fun exercise23(x:Int): Double {
+    var result: Double = 0.0
+    if(-2 <= x && 2 >= x) {
+        result = -x.toDouble()
+    } else {
+        result = 2.toDouble()
+    }
+    return result
+}
+
+fun main() {
+    var x:Int = 1
+    var result:Double = exercise23(x)
+    if(result == 1.0) {
+        println("test1 is okay")
+    } else {
+        println("test1 is not okay")
+    }
+    
+    x = -2
+    result = exercise23(x)
+    if(result == -1.0) {
+        println("test2 is okay")
+    } else {
+        println("test2 is not okay")
+    }
+    
+    x = -5
+    result = exercise23(x)
+    if(result == -4.0) {
+        println("test3 is okay")
+    } else {
+        println("test3 is not okay")
+    }
+}
