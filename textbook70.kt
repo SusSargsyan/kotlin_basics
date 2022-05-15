83f6b04573df81ccbaf975a0d20beb52ad6c1b47
SusSargsyan <100150526+SusSargsyan@users.noreply.github.com>
2022-05-15 23:28:50 +0400

    Create textbook70.kt

---
+++ textbook70.kt
@@ -0,0 +1,13 @@
+fun main() {
+    var num = 527
+    var a = 5
+    var b = 2
+    var c = 7
+    if(a > b && a > c) {
+        print(a)
+    } else if(b > a && b > c) {
+        print(b)
+    } else {
+        print(c)
+    } 
+}
