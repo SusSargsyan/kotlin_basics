a3ffed610fdfe6ef04eabeb7381d9c52ffbdd627
SusSargsyan <100150526+SusSargsyan@users.noreply.github.com>
2022-06-29 20:34:00 +0400

    Update textbook45.kt

--- textbook45.kt
+++ textbook45.kt
@@ -7,7 +7,7 @@ fun main() {
     val b:Int = 2
     val c:Int = 4
     val d:Int = 1
-   if(a <= b + c + d) {
+   if(a == b + c + d) {
           println(true)
     } else {
           println(false)
