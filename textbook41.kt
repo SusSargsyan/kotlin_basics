b6137a5ce346f7d062b729d692d7e31d5cf5056f
SusSargsyan <100150526+SusSargsyan@users.noreply.github.com>
2022-08-15 22:10:25 +0400

    Update textbook41.kt

--- textbook41.kt
+++ textbook41.kt
@@ -3,17 +3,21 @@
  */
 
 fun main() {
-    val a:Int = 3
-    val b:Int = 8
-    val c:Int = 6
-    val d:Int = 7
-    if(a < b) {
+    val a:Int = 100
+    val b:Int = 101
+    val c:Int = 102
+    val d:Int = 103
+    if(d > c) {
+        println(d)
+    } else if(c > d) {
+        println(c)
+    } else if(c > b) {
+        println(c)
+    } else if(b > c) {
         println(b)
-    } else if(c < a) {
-          println(a)
-    } else if(d > c) {
-          println(d)
+    } else if(b > a) {
+        println(b)
     } else {
-          println(a)
+        println(a)
     }
 }
