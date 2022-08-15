b04a478659729c9eeb7e181fd09fb4d0fa007e87
SusSargsyan <100150526+SusSargsyan@users.noreply.github.com>
2022-08-15 23:15:57 +0400

    Update textbook43.kt

--- textbook43.kt
+++ textbook43.kt
@@ -7,14 +7,8 @@ fun main() {
     val b:Int = 1
     val c:Int = 6
     val d:Int = 7
-   if(a == 1) {
+    if(a == 1 || b == 1 || c == 1 || d == 1) {
           println(true)
-    } else if(b == 1) {
-          println(true)
-    } else if(c == 1) {
-          println(true)
-    } else if(d == 1) {
-        println(true)
     } else {
         println(false)
     }
