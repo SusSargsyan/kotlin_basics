6f5f9279ba870764a71f863386d484b1730c51af
SusSargsyan <100150526+SusSargsyan@users.noreply.github.com>
2022-08-16 21:17:17 +0400

    Update textbook38.kt

--- textbook38.kt
+++ textbook38.kt
@@ -6,8 +6,7 @@ fun main() {
     var a1:Int = 2
     var a2:Int = 4
     var a3:Int = 8
-    var q:Int = 2
-    if(a2 == a1 * q) {
+    if((a2 == a1 * 2) && (a3 == a2 * 2)) {
         println("true")
     } else {
         println("false")
