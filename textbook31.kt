8681d80d6ba59e400187065bbe6ffa116d9c831c
SusSargsyan <100150526+SusSargsyan@users.noreply.github.com>
2022-08-16 14:41:38 +0400

    Update textbook31.kt

--- textbook31.kt
+++ textbook31.kt
@@ -3,12 +3,12 @@
  */
 
 fun main() {
-	val a:Int = 3
+    val a:Int = 3
     val b:Int = 4
     val c:Int = 1
-    if(a > b && a > c) {
+    if(a >= b && a >= c) {
         println(a)
-    } else if(b > a && b > c) {
+    } else if(b >= a && b >= c) {
         println(b)
     } else {
         println(c)
