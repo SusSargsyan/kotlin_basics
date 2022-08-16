4234de20f55a7006b45277bd5f50b26f6451de71
SusSargsyan <100150526+SusSargsyan@users.noreply.github.com>
2022-08-16 20:00:17 +0400

    Update textbook34.kt

--- textbook34.kt
+++ textbook34.kt
@@ -3,10 +3,10 @@
  */
 
 fun main() {
-	var a:Int = 5
+    var a:Int = 5
     var b:Int = 2
     var c:Int = 2
-	if(a == 2 && c == 2 || b == 2 ) {
+	if(a == 2 || c == 2 || b == 2 ) {
         println(true)
     } else{
         println(false)
