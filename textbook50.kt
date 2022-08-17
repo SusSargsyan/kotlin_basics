fa783540f8daac73daf0a40d20c8d43cc84b70e4
SusSargsyan <100150526+SusSargsyan@users.noreply.github.com>
2022-08-17 18:34:00 +0400

    Update textbook50.kt

--- textbook50.kt
+++ textbook50.kt
@@ -3,13 +3,11 @@
  */
 
 fun main() {
-    val a:Int = 15
-    val b:Int = 9
-    val c:Int = 6
-    val d:Int = 3
-    if(a > b && b > c && c > d) {
-          println("15, 9, 6, 3")
-   } else {
-        println("false")
-   }
+    val a:Int = 2
+    val b:Int = 4
+    val c:Int = 8
+    val d:Int = 16
+    val numbers = listOf("$a", "$b", "$c", "$d")
+    val reversedNumbers = numbers.asReversed()
+    println(reversedNumbers)
 }
