acd9e6984483068408fc9c414844418c9216e311
SusSargsyan <100150526+SusSargsyan@users.noreply.github.com>
2022-09-07 23:14:06 +0400

    Update textbook39.kt

--- textbook39.kt
+++ textbook39.kt
@@ -4,10 +4,10 @@
 
 
 fun main() {
-    val a = 9
-    val b = 2
-    val c = 7
-    val numsList = listOf("$a", "$b", "$c")
-    val sortAsc = numsList.sorted()
-    println(sortAsc)
+    val a = 2
+    val b = 5
+    val c = 9
+    if (a <= b && b <= c){
+        println("$a, $b, $c" )
+    }
 }
