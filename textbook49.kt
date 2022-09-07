02f16bb921027f3d68e374c6b996bdb90d61f8d4
SusSargsyan <100150526+SusSargsyan@users.noreply.github.com>
2022-09-07 23:19:34 +0400

    Update textbook49.kt

--- textbook49.kt
+++ textbook49.kt
@@ -2,13 +2,13 @@
  * Textbook - 49 problem
  */
 
-fun main() {
-    val a = 45
-    val b = 78
-    val c = 69
-    val d = 12
-    val numsList = listOf("$a","$b", "$c", "$d")
 
-    val sortAsc = numsList.sorted()
-    println(sortAsc)
+fun main() {
+    val a = 5
+    val b = 7
+    val c = 11
+    val d = 25
+    if (a <= b && b <= c && c <= d) {
+        println("$a, $b, $c, $d")
+    }
 }
